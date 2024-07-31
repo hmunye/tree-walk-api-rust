47e13770fdf2065b819a2a9502382fa1dd922a25
Huseen <136820327+hmunye@users.noreply.github.com>
2024-07-31 14:47:03 -0400

    feat: created basic axum server with health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
+
+async fn health_check() -> impl IntoResponse {
+    (StatusCode::OK, "Server Running").into_response()
+}
+
+#[tokio::main]
+async fn main() {
+    let routes = Router::new().route("/v1/healthcheck", get(health_check));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
+
+    println!("->> LISTENING on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, routes.into_make_service()).await.unwrap()
 }
